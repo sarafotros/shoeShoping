170b48b7bf15228c233d2a39a76b8c46efe49166
Sara Fotros <sarafotros@gmail.com>
2020-08-09 15:50:06 +0100

    logo added, menu page start

--- src/components/Menus/desktop/index.js
+++ src/components/Menus/desktop/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import useStyles from './index.styles'
+
+import BrandLogo1 from '../../../assets/images/brand5.png'
+import { Button } from '@material-ui/core'
 
 export default function Desktop() {
-  return <div>Desktop</div>
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <div className={classes.brandDiv}>
+        <img className={classes.brandImg} src={BrandLogo1} alt="brand-logo" />
+      </div>
+      <div className={classes.menuDiv}>
+        <Button>HOME</Button>
+      </div>
+    </div>
+  )
 }
---
+++ src/components/Menus/desktop/index.styles.js
@@ -0,0 +1,21 @@
+import { makeStyles } from '@material-ui/core'
+
+export default makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: 60,
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  brandDiv: {
+    width: 150
+  },
+  brandImg: {
+    width: '100%'
+  },
+  menuDiv: {
+    display: 'flex'
+  }
+}))
