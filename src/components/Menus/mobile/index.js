3939cb24153bff9e35fda95a8eb709f0aecb8db7
Sara Fotros <sarafotros@gmail.com>
2020-08-16 22:24:33 +0100

    menu btn in mobile

--- src/components/Menus/mobile/index.js
+++ src/components/Menus/mobile/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import useStyles from './index.styles'
+import MenuIcon from '../../../assets/images/menu2.svg'
+import brandIcon from '../../../assets/images/brand4.png'
+import CartButton from '../desktop/CartButton'
 
 export default function Mobile() {
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <div></div>
+      <div className={classes.rightMenu}>
+        <img src={MenuIcon} alt="menu" className={classes.menuIcon} />
+      </div>
+      <div className={classes.centerMenu}>
+        <img src={brandIcon} alt="menu" className={classes.brandIcon} />
+      </div>
+      <div className={classes.leftMenu}>
+        <CartButton />
+      </div>
     </div>
   )
 }
--- src/components/Menus/mobile/index.styles.js
+++ src/components/Menus/mobile/index.styles.js
@@ -3,12 +3,39 @@ import { makeStyles } from '@material-ui/core'
 export default makeStyles((theme) => ({
   root: {
     width: '100%',
-    height: 60,
-    padding: 10,
+    height: 70,
+    padding: 5,
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#131a21',
     boxShadow: '1px 0px 3px 0px #3b3939',
+  },
+  rightMenu: {
+    width: 60,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerMenu: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  leftMenu: {
+    width: 60,
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menuIcon: {
+    width: 40,
+    height: 40,
+  },
+  brandIcon: {
+    width: 140,
   },
 }))
